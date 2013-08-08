e212662499e2461fede9a1f0c784807847b85596
kakawajazz <e@filenko.su>
2013-08-08 19:02:34 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+
+
+class settings(object):
+    phoneNumber = '+375291111111'
+    apiId = '42ea2203-a777-a3c4-1d99-3fef302c7f92'
+    trackFolder = os.path.join(os.path.expanduser('~/www/python/sms/tracks/'))
+
+    items = [
+        ['CJ420541567US', 'Longboard deck', 'international'],
+        #['RQ154037942SG', 'Something', 'local'],
+    ]
